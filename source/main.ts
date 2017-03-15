f18e472ff193c41cdf73dcc87b17a8d4f3f20615
Jacob Poul Richardt <jacob@poulrichardt.dk>
2017-03-16 00:52:31 +0100

    Added texture loading.

--- source/main.ts
+++ source/main.ts
@@ -1,12 +1,14 @@
 import * as pixi from "pixi.js";
 
-export default class Main
+export class Main
 {
 	private renderer:pixi.CanvasRenderer|pixi.WebGLRenderer;
+	private stage:pixi.Container;
 
 	constructor()
 	{
 		this.createRenderer();
+		this.loadTextures();
 	}
 
 	private createRenderer():void
@@ -14,9 +16,29 @@ export default class Main
 		this.renderer = pixi.autoDetectRenderer(150, 150);
 		this.renderer.view.style.position = "absolute";
 		this.renderer.view.style.display = "block";
+		this.renderer.autoResize = true;
 		this.renderer.resize(800, 600);
+
+		this.stage = new pixi.Container();
+		this.renderer.render(this.stage);
+
 		document.body.appendChild(this.renderer.view);
+	}
+
+	private loadTextures():void
+	{
+		pixi.loader
+			.add("images/floor.png")
+			.load(()=> {
+				let sprite = new PIXI.Sprite(PIXI.loader.resources["images/floor.png"].texture);
+				let sprite2 = new PIXI.Sprite(PIXI.loader.resources["images/floor.png"].texture);
+				sprite2.x = 32
+
+				this.stage.addChild(sprite);
+				this.stage.addChild(sprite2);
+				this.renderer.render(this.stage);
+			})
 	}
 }
 
-var main = new Main();
+export default new Main();
